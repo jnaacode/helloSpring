4197bcc931cf09aa919b5c1814c7d5d3cb2b608a
pja <jnaacode@gmail.com>
2023-11-09 16:11:54 +0900

    2023-11-09  test 레포지토리

--- src/test/java/hello/helloSpring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/helloSpring/repository/MemoryMemberRepositoryTest.java
@@ -1,2 +1,24 @@
-package hello.helloSpring.repository;public class MemoryMemberRepositoryTest {
+package hello.helloSpring.repository;
+
+import hello.helloSpring.controller.domain.Member;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+        Member result = repository.findById(member.getId()).get();
+//        org.junit.jupiter.api.Assertions
+//        Assertions.assertEquals(member, null); // 실패
+//        Assertions.assertEquals(member, result); // 성공
+        assertThat(member).isEqualTo(result);
+    }
 }
