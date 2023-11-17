5c4ff682cb0c5678b746920f5d1c8437299eec1b
pja <jnaacode@gmail.com>
2023-11-17 23:51:32 +0900

    2023-11-17 테스트 진행

--- src/main/java/hello/helloSpring/service/memberService.java
+++ src/main/java/hello/helloSpring/service/memberService.java
@@ -1,2 +1,39 @@
-package hello.helloSpring.service;public class memberService {
+package hello.helloSpring.service;
+
+import hello.helloSpring.controller.domain.Member;
+import hello.helloSpring.repository.MemberRepository;
+import hello.helloSpring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class memberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /*회원 가입 */
+    public Long join(Member member){
+
+        // 같은 이름이 있는 중복 회원 x
+        extracted(member);
+
+        memberRepository.save(member);
+        return  member.getId();
+    }
+
+    private void extracted(Member member) {
+        memberRepository.findyByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /*전체 회원 조회*/
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
 }
---
+++ src/test/java/hello/helloSpring/service/memberServiceTest.java
@@ -0,0 +1,73 @@
+package hello.helloSpring.service;
+
+import hello.helloSpring.controller.domain.Member;
+import hello.helloSpring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+class memberServiceTest {
+
+    memberService memberService = new memberService();
+    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+
+
+    @AfterEach
+    public void afterEach(){
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void 회원가입() {
+
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+
+    }
+
+
+    @Test
+    public void 중복회원예약(){
+
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+        /* //then
+        try{
+            memberService.join(member2);
+            fail();
+        }catch (IllegalStateException e){
+            // assertThat을 통해 두 객체가 같은지 확인한다.
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        }*/
+
+
+    }
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
