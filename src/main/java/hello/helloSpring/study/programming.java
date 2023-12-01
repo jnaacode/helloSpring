4a098a1fe2249e12866e73bd73a239501aec7833
pja <jnaacode@gmail.com>
2023-12-01 16:31:29 +0900

    2023-12-1 1장.프로그래밍이란

---
+++ src/main/java/hello/helloSpring/study/programming.java
@@ -0,0 +1,24 @@
+package hello.helloSpring.study;
+
+public class programming {
+    // [접근제한자] [class] [클래스명]
+
+
+    public int subtract(int num1, int num2){
+
+        int sum = num1-num2;
+        return sum;
+    }
+
+    public int multiply(int num1, int num2) {
+
+        int result =num1 * num2;
+        return result;
+    }
+    public int divide(int num1, int num2) {
+        int result=num1 / num2;
+        return result;
+    }
+}
+
+
