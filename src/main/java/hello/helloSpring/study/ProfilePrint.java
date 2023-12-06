8b862c5cf83e7a9c7af72e1e7165fd6b6448434b
jnaacode@gmail.com <jnaacode@gmail.com>
2023-12-06 23:08:01 +0900

    2023-12-06 문제 풀이

--- src/main/java/hello/helloSpring/study/ProfilePrint.java
+++ src/main/java/hello/helloSpring/study/ProfilePrint.java
@@ -1,2 +1,47 @@
-package hello.helloSpring.study;public class ProfilePrint {
+package hello.helloSpring.study;
+
+public class ProfilePrint {
+    byte age;
+    String name;
+    boolean isMarried;
+
+    public static void main(String [] args){
+        ProfilePrint profilePrint = new ProfilePrint();
+
+        byte age=20;
+        String name="Min";
+        boolean isMarried=true;
+
+
+        profilePrint.setAge(age);
+        profilePrint.setName(name);
+        profilePrint.setMarried(isMarried);
+
+    }
+
+    public void setAge(byte age){
+
+    }
+
+    public byte getAge(){
+        return age;
+    }
+
+    public void setName(String name){
+
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setMarried(boolean flag){
+
+    }
+
+    public boolean isMarried(){
+        return isMarried;
+    }
+
+
 }
--- src/main/java/hello/helloSpring/study/car.java
+++ src/main/java/hello/helloSpring/study/car.java
@@ -1,2 +1,76 @@
-package hello.helloSpring.study;public class car {
+package hello.helloSpring.study;
+
+import org.springframework.context.annotation.Profile;
+
+public class car {
+    // 클래스는 상태와 행위를 가질 수 있다.
+
+    public car(){
+        // 생성자(객체 생성하기 위해 필요)
+    }
+    int speed;
+    int distance;
+    String color;
+    // 클래스(car)에 대한 상태
+
+    public void speedUp(){
+        // 클래스에 대한 행위
+        speed = speed+5;
+    }
+
+    public void breakDown(){
+        // 클래스에 대한 행위
+        speed=speed-10;
+    }
+
+    public int getCurrentspeed(){
+        return speed;
+    }
+
+    public class CarManager{
+        public static void main(String [] args){
+            car dogCar = new car();
+            car cowCar = new car();
+            cowCar.speedUp();
+
+        }
+    }
+
+
+    // 연습 문제
+
+    public class Profile{
+        String name;
+        int age;
+
+        public void main(String[] args){
+            Profile profile = new Profile();
+            profile.setName("Min");
+            profile.setAge(20);
+            profile.printName();
+            profile.printAge();
+
+
+        }
+
+        public void printName(){
+            System.out.println("My name is"+name);
+        }
+
+        public void printAge(){
+            System.out.println("My age is"+age);
+        }
+
+        public void setName(String str){
+            name= str;
+        }
+
+        public void setAge(int val){
+            age = val;
+        }
+
+
+    }
+
+
 }
